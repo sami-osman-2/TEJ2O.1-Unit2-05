ad24e7b9cbb2e051b8ed00883bce6896e75d89fc
sami-osman-2 <sami.osman@stu.ocsb.ca>
2025-09-23 13:03:44 -0400

    Shows temperature in kelvin

--- main.ts
+++ main.ts
@@ -8,4 +8,21 @@
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-//comment
+// Converts temperature to kelvin
+let temperature: number
+let temperatureValueKelvin: number
+
+// Gets temperature value
+temperature = input.temperature()
+temperatureValueKelvin = Math.round(temperature + 273.15)
+
+
+// When button A is pressed...
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showString('The temperature is' + temperatureValueKelvin.toString() + 'K')
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+
+})
