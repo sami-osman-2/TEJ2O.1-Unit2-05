977eb5713fe3081be9f90ed849bd10ca4a3d64dd
sami-osman-2 <sami.osman@stu.ocsb.ca>
2025-11-26 23:24:09 -0500

    finished

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,25 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Copyright (c) 2025 MTHS All rights reserved
+Created by: Sami Osman
+Created on: Nov 2025
+Program gets the current temperature, converts it to Kelvin, rounds it, and displays it.
 """
-
 from microbit import *
 
+# variable for temperature
+temp_c = 0
+temp_k = 0
 
-display.scroll("Hello, World!")
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.was_pressed():
+        # get temp in Celsius
+        temp_c = temperature()
+        
+        # convert to Kelvin
+        temp_k = temp_c + 273.15
+        temp_k = round(temp_k)
+        
+        # output answer
+        display.scroll(str(temp_k) + " K")
